e45f9c9c5f9e58ac81ba140b608c182f15f5e7f4
mohamed abass tawana <aminabass987@gmail.com>
2025-05-24 23:05:49 +0300

    cli

--- lib/cli.py
+++ lib/cli.py
@@ -5,18 +5,18 @@ def menu():
     print("1. List all department")
     print("2. Find department by id")
     print("3. Find department by name")        
-# def main():
-#     while True:
-#         menu()
-#         choice = input(":")
-#         if choice == "0":
-#             exit_application()
-#         elif choice == "1":
-#             list_all_departments()
-#         elif choice == "2":
-#             get_department_by_id()
-#         elif choice == "3":
-#             get_department_by_name()
-#         else:
-#             print("Invalid choice: Try again")
-# main()
+def main():
+    while True:
+        menu()
+        choice = input(":")
+        if choice == "0":
+            exit_application()
+        elif choice == "1":
+            list_all_departments()
+        elif choice == "2":
+            get_department_by_id()
+        elif choice == "3":
+            get_department_by_name()
+        else:
+            print("Invalid choice: Try again")
+main()
