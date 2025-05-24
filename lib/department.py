a64a1eeaad918e289dcc5b3a98a809fb0e774873
mohamed abass tawana <aminabass987@gmail.com>
2025-05-24 22:46:06 +0300

    department

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,15 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute(
+            """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                location TEXT NOT NULL
+            )
+            """
+        )
+        CONN.commit()
