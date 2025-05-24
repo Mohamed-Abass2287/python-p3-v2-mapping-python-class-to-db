1d51dc0f5e9d3da24278e1251e1a7352686068aa
mohamed abass tawana <aminabass987@gmail.com>
2025-05-24 22:48:09 +0300

    add

--- lib/department.py
+++ lib/department.py
@@ -1,27 +1,3 @@
-from __init__ import CURSOR, CONN
 
-
-class Department:
-
-    def __init__(self, name, location, id=None):
-        self.id = id
-        self.name = name
-        self.location = location
-
-    def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
-    @classmethod
-    def create_table(cls):
-        CURSOR.execute(
-            """
-            CREATE TABLE IF NOT EXISTS departments (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                location TEXT NOT NULL
-            )
-            """
-        )
-        CONN.commit()
 
     @classmethod
-    
